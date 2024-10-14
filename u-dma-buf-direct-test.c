4e76969b1826a7fa106f50e7721aca75dac73396
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2024-10-14 15:20:26 +0900

    [add] command option to u-dma-buf-direct-test to specify the target device.

--- u-dma-buf-direct-test.c
+++ u-dma-buf-direct-test.c
@@ -6,6 +6,7 @@
 #include        <stdlib.h>
 #include        <errno.h>
 #include        <unistd.h>
+#include        <getopt.h>
 #include        <sys/time.h>
 #include        <sys/types.h>
 #include        <sys/stat.h>
@@ -61,9 +62,11 @@ void write_buf_test(void* udmabuf_map, unsigned int udmabuf_size, int o_direct)
     close(dump_fd);
 }
 
-void main()
+void main(int argc, char* argv[])
 {
-    unsigned char  attr[1024];
+    char           device_name[256];
+    char           file_name[1024];
+    char           attr[1024];
     int            udmabuf_fd;
     void*          udmabuf_map;
     unsigned int   udmabuf_size;
@@ -71,8 +74,37 @@ void main()
     int            quirk_mmap_mode = -1;
     char*          driver_version  = NULL;
     int            try_count       = 5;
+    int            verbose         = 0;
+    int            opt;
+    int            optindex;
+    struct option  longopts[] = {
+      { "name"      , required_argument, NULL, 'n'},
+      { "t"         , required_argument, NULL, 't'},
+      { "verbose"   , no_argument      , NULL, 'v'},
+      { NULL        , 0                , NULL,  0 },
+    };
 
-    if ((udmabuf_fd  = open("/sys/class/u-dma-buf/udmabuf0/driver_version", O_RDONLY)) != -1) {
+    strncpy(device_name, "udmabuf0", sizeof(device_name));
+    while ((opt = getopt_long(argc, argv, "n:", longopts, &optindex)) != -1) {
+      switch (opt) {
+        case 'n':
+          strncpy(device_name, optarg, sizeof(device_name));
+          break;
+        case 't':
+          scanf(optarg, "%d", &try_count);
+          break;
+        case 'v':
+          verbose = 1;
+          break;
+        default:
+          printf("error options\n");
+          break;
+      }
+    }
+    printf("device=%s\n", device_name);
+
+    sprintf(file_name, "/sys/class/u-dma-buf/%s/driver_version", device_name);
+    if ((udmabuf_fd  = open(file_name, O_RDONLY)) != -1) {
       int len;
       len = read(udmabuf_fd, attr, 1024);
       while(--len >= 0) {
@@ -83,20 +115,30 @@ void main()
       }
       driver_version = strdup(attr);
       close(udmabuf_fd);
+    } else {
+      perror(file_name);
+      exit(-1);
     }
 
-    if ((udmabuf_fd  = open("/sys/class/u-dma-buf/udmabuf0/size"     , O_RDONLY)) != -1) {
+    sprintf(file_name, "/sys/class/u-dma-buf/%s/size", device_name);
+    if ((udmabuf_fd  = open(file_name, O_RDONLY)) != -1) {
       read(udmabuf_fd, attr, 1024);
       sscanf(attr, "%d", &udmabuf_size);
       close(udmabuf_fd);
+    } else {
+      perror(file_name);
+      exit(-1);
     }
 
-    if ((udmabuf_fd  = open("/sys/class/u-dma-buf/udmabuf0/dma_coherent", O_RDONLY)) != -1) {
+    sprintf(file_name, "/sys/class/u-dma-buf/%s/dma_coherent", device_name);
+    if ((udmabuf_fd  = open(file_name, O_RDONLY)) != -1) {
       read(udmabuf_fd, attr, 1024);
       sscanf(attr, "%d", &dma_coherent);
       close(udmabuf_fd);
     }
-    if ((udmabuf_fd  = open("/sys/class/u-dma-buf/udmabuf0/quirk_mmap_mode", O_RDONLY)) != -1) {
+
+    sprintf(file_name, "/sys/class/u-dma-buf/%s/quirk_mmap_mode", device_name);
+    if ((udmabuf_fd  = open(file_name, O_RDONLY)) != -1) {
       read(udmabuf_fd, attr, 1024);
       sscanf(attr, "%d", &quirk_mmap_mode);
       close(udmabuf_fd);
@@ -118,10 +160,12 @@ void main()
       printf("quirk_mmap=page\n");
     printf("size=%d\n", udmabuf_size);
 
-    if ((udmabuf_fd  = open("/dev/udmabuf0", O_RDWR)) == -1) {
-      printf("can not open %s\n", "/dev/udmabuf0");
+    sprintf(file_name, "/dev/%s", device_name);
+    if ((udmabuf_fd  = open(file_name, O_RDWR)) == -1) {
+      perror(file_name);
       exit(-1);
     }
+
     udmabuf_map = mmap(NULL, udmabuf_size, PROT_READ|PROT_WRITE, MAP_SHARED, udmabuf_fd, 0);
     if (udmabuf_map == MAP_FAILED) {
       printf("can not mmap\n");
